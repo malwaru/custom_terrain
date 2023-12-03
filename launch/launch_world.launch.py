b10fd93f43046d82dee459ac84d8c40cc5f2b2a5
malwaru <malika.warunamal@gmail.com>
2023-12-03 21:16:54 +0100

    udated to use world

--- launch/launch_world.launch.py
+++ launch/launch_world.launch.py
@@ -3,6 +3,7 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument,IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
+from launch.substitutions import LaunchConfiguration
  
 
 def generate_launch_description():
@@ -11,13 +12,23 @@ def generate_launch_description():
     pkg_gazebo_ros=get_package_share_directory('gazebo_ros')
     pkg_gazebo_world=get_package_share_directory('custom_terrain')
     pkg_terrain_description=get_package_share_directory('custom_terrain')
-    world_file=os.path.join(pkg_gazebo_world,'src/worlds','terrain.world')
+    world_file=os.path.join(pkg_gazebo_world,'src/world','terrain.world')
+    world = LaunchConfiguration('world')
 
     # Gazebo launch the empty world
 
-    declare_world_file_cmd=DeclareLaunchArgument('world_file',default_value=world_file,description='Full path to world model file to load')
+    declare_world_file_cmd=DeclareLaunchArgument('world',default_value=world_file,description='Full path to world model file to load')
     start_gazebo = IncludeLaunchDescription(
-    PythonLaunchDescriptionSource(os.path.join(pkg_gazebo_ros, 'launch', 'gazebo.launch.py')),)
+    PythonLaunchDescriptionSource(os.path.join(pkg_gazebo_ros, 'launch', 'gazebo.launch.py')),
+     launch_arguments={'world': world}.items())
+#     start_gazebo_server_cmd = IncludeLaunchDescription(
+#     PythonLaunchDescriptionSource(os.path.join(pkg_gazebo_ros, 'launch', 'gzserver.launch.py')),
+#     launch_arguments={'world': world}.items())
+ 
+#   # Start Gazebo client    
+#     start_gazebo_client_cmd = IncludeLaunchDescription(
+#     PythonLaunchDescriptionSource(os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py')),
+#     )
 
     # Launch the terrain spawner launch file
     spawn_terrain=IncludeLaunchDescription(
@@ -32,6 +43,8 @@ def generate_launch_description():
     
     ld.add_action(declare_world_file_cmd)
     ld.add_action(start_gazebo)
+    # ld.add_action(start_gazebo_server_cmd)
+    # ld.add_action(start_gazebo_client_cmd)
     ld.add_action(spawn_terrain)
 
 
